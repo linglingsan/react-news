f6a8ed4198711c3dfeeaaaeb77612994fc575695
linglingsan <zhangchunlin5614@163.com>
2019-04-06 23:27:07 +0800

    mobile tab

--- src/MobileIndex/index.tsx
+++ src/MobileIndex/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import MobileHeader from "../components/MobileHeader";
 import MobileFooter from "../components/MobileFooter";
-
+import {Tabs} from 'antd';
+const TabPane = Tabs.TabPane;
 class MobileIndex extends React.Component{
     render(): React.ReactNode {
         return (
             <div>
                 <MobileHeader/>
+                <Tabs>
+                    <TabPane tab="头条" key="1"></TabPane>
+                    <TabPane tab="社会" key="2"></TabPane>
+                    <TabPane tab="国内" key="3"></TabPane>
+                    <TabPane tab="国际" key="4"></TabPane>
+                    <TabPane tab="娱乐" key="5"></TabPane>
+                </Tabs>
                 <MobileFooter/>
             </div>
         );
